1164f405b02d8992a2175f0233c3d265242c29a8
mohamedelhacen <hacen13037@gmail.com>
2022-11-27 10:18:22 +0300

    adding flask app

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for, request
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+app.config['DEBUG']=True
+
+
+pipeline = joblib.load('pipeline.pkl')
+model = joblib.load('forest_reg.pkl')
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+	res = ''
+	if request.method == "POST":
+		longitude = request.form.get("longitude")
+		latitude = request.form.get("latitude")
+		age = request.form.get("age")
+		totalrooms = request.form.get("totalrooms")
+		totalbedrooms = request.form.get("totalbedrooms")
+		population = request.form.get("population")
+		households = request.form.get("households")
+		income = request.form.get("income")
+		oceanproximity = request.form.get("oceanproximity")
+
+		columns = ['longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 
+		'households', 'median_income', 'ocean_proximity']
+		vals = [[longitude], [latitude], [age], [totalrooms], [totalbedrooms], [population], [households],
+		[income], [oceanproximity]]
+
+		housing = pd.DataFrame.from_dict(dict(zip(columns, vals)))
+		housing.to_csv('static/housing.csv', index=False)
+
+		housing_prepared = pipeline.transform(housing)
+		result = model.predict(housing_prepared)
+
+		return render_template('index.html', res=result, cols=columns, rows=vals)
+
+	return render_template('index.html', res=res)
